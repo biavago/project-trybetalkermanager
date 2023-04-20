43cff17136d09785c4aa9aeacfeb20e08579de7d
beatriz <beatriz.evago@gmail.com>
2023-04-20 10:21:55 +0100

    Requisito 1 refatorado

--- src/readTalkerFile.js
+++ src/readTalkerFile.js
@@ -1,7 +1,10 @@
 const fs = require('fs').promises;
+const path = require('path');
+
+const talkerFilePath = path.resolve(__dirname, 'talker.json');
 
 const readTalkers = async () => {
-  const talkerFile = await fs.readFile('./talker.json', 'utf-8');
+  const talkerFile = await fs.readFile(talkerFilePath, 'utf-8');
   return JSON.parse(talkerFile);
 };
 
